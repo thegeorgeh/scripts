ecbcd7268f23e775facff1c3b6af04ce8d4b7ba1
George <george@george.Home>
2020-02-09 21:51:20 +0000

    utter nightmare now finished

--- bash/manga.sh
+++ bash/manga.sh
@@ -1,34 +1,43 @@
 
 #!/bin/bash
 
-mangasite=("https://mangakakalot.com/" "https://m.fanfox.net")
-manga=("6 worlds of cultivation" "i am the sorcerer king" "jishou! heibon mazoku no eiyuu life: b-kyuu mazoku nano ni cheat dungeon wo tsukutteshimatta kekka" "maougakuin no futekigousha" "martial peak" "mastery of legendary realms" "onepunch-man" "release that witch" "shinka no mi" "solo Leveling" "tensei ouji wa daraketai")
+mangasite=("https://mangakakalot.com/" "https://m.fanfox.net/")
+manga=("6 Worlds of Cultivation" "I am the Sorcerer King" "Jishou! Heibon Mazoku no Eiyuu Life: B-Kyuu Mazoku Nano ni Cheat Dungeon wo Tsukutteshimatta Kekka" "Maougakuin no Futekigousha" "Martial Peak" "Mastery of Legendary Realms" "Onepunch-Man" "Release That Witch" "Shinka no Mi" "Solo Leveling" "Tensei Ouji wa Daraketai" "Black Clover")
 
 msLen=${#mangasite[@]}
 mLen=${#manga[@]}
 
 text="\n"
+counter=0
 
 for (( i=0; i<$msLen; i++ ))
 do
-    #echo ${mangasite[$i]}
+    
     text+="${mangasite[$i]}\n"
     
-    op=$( curl -s --ipv6 ${mangasite[$i]} | tr '[:upper:]' '[:lower:]' )
+    op=$( curl -s "${mangasite[$i]}" ) 
+   
+    
     for (( j=0; j<$mLen; j++ ))
     do
-	if [[ $op == *${manga[$j]}* ]]; 
+	if [[ "$op" == *"\"${manga[$j]}\""* ]];  
 	then
-#	    #echo ${manga[$j]}
     	    text+="${manga[$j]}\n"
+	    let counter+=1
 	fi
- #    
     done
-    #echo ''
     text+="\n"
 done
 
 printf "$text"
-#notify-send "$text"
+printf "$counter\n"
+
+if (( $counter > 0 ))
+then
+    printf "$text" > ~/Desktop/manga.txt
+    notify-send "New Manga Available"
+else
+    printf "" > ~/Desktop/manga.txt
+fi
 
 	
--- powershell/manga.ps1
+++ powershell/manga.ps1
@@ -1,18 +1,18 @@
 $mangasite = "https://mangakakalot.com/", "https://m.fanfox.net"
-$manga = "6 worlds of cultivation", "i am the sorcerer king", "jishou! heibon mazoku no eiyuu life: b-kyuu mazoku nano ni cheat dungeon wo tsukutteshimatta kekka", "maougakuin no futekigousha", "martial peak", "mastery of legendary realms", "onepunch-man", "release that witch", "shinka no mi", "Solo Leveling"
+$manga = "6 Worlds of Cultivation", "I am the Sorcerer King", "Jishou! Heibon Mazoku no Eiyuu Life: B-Kyuu Mazoku Nano ni Cheat Dungeon wo Tsukutteshimatta Kekka", "Maougakuin no Futekigousha", "Martial Peak", "Mastery of Legendary Realms", "Onepunch-Man", "Release That Witch", "Shinka no Mi", "Solo Leveling", "Tensei Ouji wa Daraketai", "black clover"
 
 $text = ""
 #$counter = 0
 
 for ( $i = 0; $i -le $mangasite.length - 1; $i++ ) {
     #Write-Output $mangasite[$i]
-    $text += $mangasite + "`r`n"  
+    $text += $mangasite[$i] + "`r`n"  
     $sitecontent = Invoke-RestMethod $mangasite[$i]
     for ( $j = 0; $j -le $manga.length; $j++ ) {
     	if ( $sitecontent -Match $manga[$j] ) {
    	     	#Write-Output $manga[$j]
    		$text += $manga[$j] + "`r`n"
-		#$counter++
+		$counter++
 	}
     }
     $text += "`r`n"
@@ -20,15 +20,17 @@ for ( $i = 0; $i -le $mangasite.length - 1; $i++ ) {
 }
 
 Write-Output $text
-
-#if ( $counter -mt 0 ) {
-#   Add-Type -AssemblyName System.Windows.Forms
-#   $global:balloon = New-Object System.Windows.Forms.NotifyIcon
-#   $path = (Get-Process -id $pid).Path
-#   $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-#   $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-#   $balloon.BalloonTipText = $text
-#   $balloon.BalloonTipTitle = Manga Update
-#   $balloon.Visible = $true
-#   $balloon.ShowBalloonTip(10000)
-#}
+$counter-=2
+Write-Output $counter
+if ( $counter -mt 0 ) {
+   Write-Output $text > C:\manga.txt
+   Add-Type -AssemblyName System.Windows.Forms
+   $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+   $path = (Get-Process -id $pid).Path
+   $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+   $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+   $balloon.BalloonTipText = Watch New Manga About
+   $balloon.BalloonTipTitle = Manga Update
+   $balloon.Visible = $true
+   $balloon.ShowBalloonTip(10000)
+}
