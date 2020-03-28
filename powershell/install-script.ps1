fc316b080022acb8704e5dd005320c9e63632452
thegeorgeh <47025995+thegeorgeh@users.noreply.github.com>
2020-03-28 17:39:39 +0000

    Removal of Windows 10 pre-installed application script added to install-script.ps1

--- powershell/install-script.ps1
+++ powershell/install-script.ps1
@@ -16,3 +16,34 @@ If($isHomeInstall) {
   choco install spotify
   choco install steam
 }
+
+# Remove Windows 10 pre-installed application
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *Asphalt8Airborne* | Remove-AppxPackage
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+Get-AppxPackage *CandyCrushSodaSaga* | Remove-AppxPackage
+Get-AppxPackage *Facebook* | Remove-AppxPackage
+Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage
+Get-AppxPackage *gethelp* | Remove-AppxPackage
+Get-AppxPackage *HolographicFirstRun* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *Messaging* | Remove-AppxPackage
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+Get-AppxPackage *Office.Sway* | Remove-AppxPackage
+Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage
+Get-AppxPackage *People* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *Twitter* | Remove-AppxPackage
+Get-AppxPackage *Wallet* | Remove-AppxPackage
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+
+
