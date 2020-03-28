19764cbd7c013218f33fd558e475399d2ff1aaa6
thegeorgeh <47025995+thegeorgeh@users.noreply.github.com>
2020-03-28 13:49:45 +0000

    Update install-script.ps1

--- powershell/install-script.ps1
+++ powershell/install-script.ps1
@@ -1,8 +1,18 @@
 
+param(
+  [switch]$isHomeInstall = $false
+)
+
 # install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # applications
+choco install firefox
 choco install 7zip.install
 choco install vscode
 choco install powertoys
+
+If($isHomeInstall) {
+  choco install spotify
+  choco install steam
+}
