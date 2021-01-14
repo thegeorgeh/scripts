b3b8edb706bf68658e1f97afd91458db37078088
thegeorgeh <47025995+thegeorgeh@users.noreply.github.com>
2021-01-14 10:12:06 +0000

    added more manga to the manga list

--- powershell/manga.ps1
+++ powershell/manga.ps1
@@ -1,8 +1,14 @@
 $mangasite = "https://mangakakalot.com/", "https://m.fanfox.net" #, "https://www.mangareader.net"
-$manga =   "After Being Reborn, I Became the Strongest to Save Everyone", "Class ga Isekai Shoukan sareta Naka Ore dake Nokotta n desu ga", "Cultivation through science", "Different World Slow Life Begun at the Smith", 
-"Dosanko Gyaru Is Mega Cutei", "Hahazuka-san o xx ni Shitai.", "Himegasaki Sakurako wa Kyou mo Fubin Kawaii!", "Hishou! Heibon Mazoku no Eiyuu Life", "Mato Seihei no Slave", "My Dad Is Too Strong", "Noah's Notes", "One Piece", 
-"Ranker Who Lives A Second Time", "Release That Witch", "Re:Monster", "Rettou Hito no Maken Tsukai Sukiruboudo o Kushi Shite Saikyou ni Itaru", "Second Life Ranker", "Shingan no Yuusha", "Solo Leveling", "Tales of Demons and Gods", 
-"Tensei Shitara Dragon no Tamago Datta", "The Beginning After the End", "The New Gate", "The Other World's Wizard Does Not Chant", "Tsui no Taimashi"
+$manga = "A Sugar-Sweet Dad and A Salty-Sour Daughter", "Bodyguard of the Goddess", "Class ga Isekai Shoukan sareta Naka Ore dake Nokotta n desu ga", "Could You Turn Three Perverted Sisters Into Fine Brides?", "Craft Game No \nouryoku De Isekai Kouryaku!!", 
+"Cultivation through science", "Different World Slow Life Begun at the Smith", "Dosanko Gyaru Is Mega Cutei", "Eden's Zero", "Extraordinary Son-In-Law", "Hahazuka-san o xx ni Shitai.", "Hakui no Eiyuu", "Henkyou Gurashi no Maou", 
+"Himegasaki Sakurako wa Kyou mo Fubin Kawaii!", "Hishou! Heibon Mazoku no Eiyuu Life", "I am the Final Boss", "I Have Twin Girlfriends", "Isekai de tadahitori no mamono tsukai", "Isekai Monster Breeder", "I, the Strongest Demon, Have Regained My Youth?!", 
+"It's Sudden, but I cam to Another World! But I hope to live Safely", "Jishou! Heibon Mazoku no Eiyuu Life", "Kitaku Tochuu de Yome to Musume ga dekita n dakedo, Dragon datta", "Magical★Explorer", "Mahou Shoujo Gakuen no Suketto Kyoushi", 
+"Makai Kaeri no Rettou Nouryokusha", "Maou Gun Saikyou no Majutushi wa Ningen datta", "Maou ni Natta node", "Maou-sama no Machizukuri!", "Mato Seihei no Slave", "Mo Shou Jian Sheng", "Moto Sekai Ichi'i Subchara Ikusei Nikki", "Mushoku no Eiyuu", 
+"My Dad Is Too Strong", "My disciples are super gods", "My Master Is A Deity", "My Roommate Isn't From This World", "Ningen Dakedo Maougun Shitennou ni Sodaterareta Ore wa", "Nise Seiken Monogatari: Osananajimi no Seijo o Uttara Michizure ni Sareta", 
+"Noah's Notes", "One Piece", "Ranker Who Lives A Second Time", "Release That Witch", "Re:Monster", "Rettou Hito no Maken Tsukai Sukiruboudo o Kushi Shite Saikyou ni Itaru", "Risou no Himo Seikatsu", "Second Life Ranker", "Sekai no Owari no Encore", 
+"Shijou Saikyou no Daimaou", "Shitsugyou Kenja no Nariagari", "Shingan no Yuusha", "Shin no Jitsuryoku wa Girigiri Made Kakushite Iyou to Omou", "Slave Of Black Knight", "Solo Leveling", "Tales of Demons and Gods", "Taming Master", 
+"Tensei Shitara Dragon no Tamago Datta", "The Beginning After the End", "The New Gate", "The Other World's Wizard Does Not Chant", "The Useless Tamer Will Turn into the TopUnconsciously by My Previous Life Knowledge", "The World of Moral Reversal", 
+"The World's Strongest Fighter Who Tried Too Hard Living A Leisure Life In A World Of Magic", "The Wrong Way to use Healing Magic", "Tsui no Taimashi", "World Trigger", "Yondome wa lyana Shi Zokusei Majutsushi"
 
 $text = ""
 #$counter = 0
